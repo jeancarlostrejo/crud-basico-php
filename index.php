60032edbe4f7b9755aaf8fadcdfce3bd74b8c543
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-05-20 20:48:34 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud en PHP</title>
+</head>
+<body>
+    <h1>Crud con PHP</h1>
+</body>
+</html>
