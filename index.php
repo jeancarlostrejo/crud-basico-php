febfc33f413279f31f28238140e250de15108901
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-05-20 21:05:49 -0400

    add bootstrap

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud en PHP</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Crud con PHP</h1>
+    <h1 class="text-center p-2">Crud con PHP</h1>
+    <script src="./js/bootstrap.min.js"></script>
 </body>
 </html>
